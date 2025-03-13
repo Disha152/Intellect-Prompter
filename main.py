7cc9064ef0c1c57a3636d0ffc315f1718553c01d
Disha Gupta <disha29082001@gmail.com>
2025-03-13 21:04:08 +0530

    changes made

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pandasai.responses import ResponseParser
 from data import load_data
 import google.generativeai as genai
 import pandas as pd
-import plotly.express as px
 import speech_recognition as sr  
 
 # Load environment variables
